f81396746a7ededf8727a69dbfb1402846c643fe
Hyejin Ha <gpwlsdlsms50@naver.com>
2022-06-27 20:50:31 +0900

    selenium test 전 commit

---
+++ src/main/java/com/bcc/service/MapService.java
@@ -0,0 +1,9 @@
+package com.bcc.service;
+
+import java.io.IOException;
+
+public interface MapService {
+	public void tourLists() throws IOException;
+	public void restaurantLists() throws IOException;
+	
+}
