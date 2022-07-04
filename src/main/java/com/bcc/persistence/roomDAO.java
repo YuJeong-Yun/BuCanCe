16de6017d0648d4127b1c0ec15ac0027676d45b2
dokky41 <kld9223@kakao.com>
2022-07-04 12:14:52 +0900

    update : 재업로드

--- src/main/java/com/bcc/persistence/roomDAO.java
+++ src/main/java/com/bcc/persistence/roomDAO.java
@@ -9,21 +9,5 @@ import com.bcc.domain.roomSearch;
 
 public interface roomDAO {
 
-	public JSONArray roomDAO();
 
-	public JSONArray roomDAO(roomSearch rs);
-	
-	public JSONArray roomDetailDAO(String bno);
-	
-	public JSONArray roomDetailDAO2(String bno);//상세정보 (서비스)
-
-	public JSONArray roomDetailDAO3(String bno);//상세정보 ( 주의사항)
-
-	public JSONArray roomDetailDAO4(String bno);//상세정보 (주변안내)
-
-	public JSONArray roomDetailDAO5(String bno);// 상세정보 (상세정보)
-
-	public JSONArray roomPriceDAO(String bno);
-
-	public JSONArray roomReserveDAO(String bno,roomDate rd,String ano);
 }
