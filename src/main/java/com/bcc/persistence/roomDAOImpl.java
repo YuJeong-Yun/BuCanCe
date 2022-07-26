3e1280c99886006f072b8071084b03169123e366
dokky41 <kld9223@kakao.com>
2022-07-26 17:07:02 +0900

    pr용

--- src/main/java/com/bcc/persistence/roomDAOImpl.java
+++ src/main/java/com/bcc/persistence/roomDAOImpl.java
@@ -9,7 +9,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
-import com.bcc.domain.KakaoVO;
 import com.bcc.domain.roomPayVO;
 import com.bcc.domain.roomRefundVO;
 
